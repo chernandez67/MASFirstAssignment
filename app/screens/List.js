75fba6eba27b6848d56a5c39da653d66e2149333
chernandez67 <chernandez67@gatech.edu>
2023-09-12 22:50:21 -0400

    Change delete button styling

--- app/screens/List.js
+++ app/screens/List.js
@@ -5,30 +5,30 @@ import {doc, deleteDoc, onSnapshot, addDoc, collection, updateDoc } from 'fireba
 import { Entypo, AntDesign } from '@expo/vector-icons'; 
 
 const List = ({ navigation }) => {
-    const [todos, setTodos] = useState([]);
-    const [todo, setTodo] = useState('');
-    const todoCollectionRef = collection(FIRESTORE_DB, 'items');
+    const [items, setItems] = useState([]);
+    const [item, setItem] = useState('');
+    const itemCollectionRef = collection(FIRESTORE_DB, 'items');
 
     useEffect(() => {
-        const subscriber = onSnapshot(todoCollectionRef, {
+        const subscriber = onSnapshot(itemCollectionRef, {
             next: (snapshot) => {
-                const todos = [];
+                const items = [];
                 snapshot.docs.forEach((doc) => {
-                    todos.push({
+                    items.push({
                         id: doc.id,
                         ...doc.data(),
                     });
                 });
-                setTodos(todos)
+                setItems(items)
             }
         });
 
         return () => subscriber();
     }, []);
 
-    const addTodo = async() => {
-        const doc = await addDoc(todoCollectionRef, {title: todo, quantity: 1});
-        setTodo('');
+    const addItem = async() => {
+        const doc = await addDoc(itemCollectionRef, {title: item, quantity: 1});
+        setItem('');
     }
 
     const deleteItem = async(id) => {
@@ -41,14 +41,14 @@ const List = ({ navigation }) => {
         updateDoc(itemDoc, {quantity: newQuantity});
     }
 
-    const renderTodo = ({item}) => {
+    const renderItem = ({item}) => {
 
         return (
-            <View style={styles.todoContainer}>
-                <Text  style={styles.todoText}>{item.title}: {item.quantity}</Text>
+            <View style={styles.itemContainer}>
+                <Text  style={styles.itemText}>{item.title}: {item.quantity}</Text>
                 <AntDesign name="plus" size={24} color="black" onPress={() => changeQuantity(item.id, item.quantity + 1)}/>
                 <AntDesign name="minus" size={24} color="black" onPress={() => changeQuantity(item.id, Math.max(1, item.quantity - 1))}/>
-                <Entypo name="trash" size={24} color="black" onPress={() => deleteItem(item.id)} />
+                <Entypo style={styles.itemRemove} name="trash" size={24} color="black" onPress={() => deleteItem(item.id)} />
             </View>
         )
     };
@@ -56,15 +56,15 @@ const List = ({ navigation }) => {
     return (
         <View style={styles.container}>
             <View style={styles.form}>
-                <TextInput style={styles.input} placeholder = "Add new item" onChangeText={(text) => setTodo(text)} value={todo} />
-                <Button onPress={() => addTodo()} title="Add Item" disabled={todo === ''}/>
+                <TextInput style={styles.input} placeholder = "Add new item" onChangeText={(text) => setItem(text)} value={item} />
+                <Button onPress={() => addItem()} title="Add Item" disabled={item === ''}/>
             </View>
-            { todos.length > 0 && (
+            { items.length > 0 && (
             <View>
                 <FlatList
-                data={todos}
-                renderItem={renderTodo}
-                keyExtractor={(todo) => todo.id}
+                data={items}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.id}
                 />
             </View>
             )}
@@ -91,15 +91,18 @@ styles = StyleSheet.create({
         padding: 10,
         backgroundColor: '#fff'
     },
-    todoContainer: {
+    itemContainer: {
         flexDirection: 'row',
         alignItems: 'center',
         backgroundColor: '#fff',
         padding: 10,
         marginVertical: 4
     },
-    todoText: {
+    itemText: {
         flex: 1,
         paddingHorizontal: 4
+    },
+    itemRemove: {
+        paddingHorizontal: 2
     }
 })
